81b35066854e836bbe552e577c7fbd2ff9f6dfb1
Murilo Lavratti <20212PF.CC0006@passofundo.ifsul.local>
2024-03-20 11:07:07 -0300

    iniciando projeto

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/compras/lpoo_sistemacompras/LPOO_SistemaCompras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.cc.lpoo.compras.lpoo_sistemacompras;
+
+/**
+ *
+ * @author 20212PF.CC0006
+ */
+public class LPOO_SistemaCompras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
